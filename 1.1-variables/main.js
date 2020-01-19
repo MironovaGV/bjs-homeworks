a251b0e225dda39d42416a0eb5dd47726c772eb4
Galina Mironova <garta89@gmail.com>
2020-01-19 22:55:34 +0300

    first homework

---
+++ 1.1-variables/main.js
@@ -0,0 +1,21 @@
+let algebra = 5;
+let geography = 4;
+let physics = 3;
+let numberOfLessons = 3;
+
+let arithmeticMean = (algebra + geography + physics) / numberOfLessons;
+
+console.log(arithmeticMean);
+
+
+let myName = 'Galina';
+let message = `Привет, мир! Меня зовут ${myName}`;
+
+console.log(message);
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+console.log(result);
