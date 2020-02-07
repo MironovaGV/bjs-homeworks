1ab2cf61b5c20601eefc1c763df7f180bd6ddb67
Galina Mironova <garta89@gmail.com>
2020-02-08 00:10:26 +0300

    homeworks four

--- 2.1-wrappers-for-primitives/task.js
+++ 2.1-wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -9,10 +11,35 @@ function calculateMortgage() {
     span.textContent = result;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+function calculateTotalMortgage(percent, contribution, amount, date) {
+    let localPercent = Number.parseInt(percent),
+        localContribution = Number.parseInt(contribution),
+        localAmount = Number.parseInt(amount);
+
+    if (isNaN(localPercent)) {
+        return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+    }
+
+    if (isNaN(localContribution)) {
+        return `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`;
+    }
+
+    if (isNaN(localAmount)) {
+        return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
+    }
+
+    let returnAmont = localAmount - localContribution;
+    let myDate = new Date(date);
+    let now = new Date();
+    let diff = ((myDate.getFullYear() - now.getFullYear()) * 12) + (myDate.getMonth() - now.getMonth());
+    let p = localPercent / 100 / 12;
+
+    let perMonth = returnAmont * (p + p / ((Math.pow((1 + p), diff)) - 1));
+    let totalAmount = parseFloat(diff * perMonth).toFixed(2);
+
+    console.log(totalAmount);
+    return totalAmount;
 }
 
 function sayHello() {
@@ -23,6 +50,9 @@ function sayHello() {
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+    if (name === '' || name === 'null' || name === 'undefined') {
+        name = 'Аноним';
+    }
+    
+    return `Привет, мир! Меня зовут ${name}.`;
 }
