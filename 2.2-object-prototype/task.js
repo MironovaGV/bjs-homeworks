36812e30c5ccb78daa93625eee8ea24f988abc27
Galina Mironova <garta89@gmail.com>
2020-02-11 00:45:57 +0300

    homeworks five

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,62 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    if (animal === undefined) {
+        return null;
+    }
+    let sound = animal.sound;
+
+    return sound;
 }
 
+
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (marks.length === 0) {
+        return 0;
+    }
+
+    let sumItem = 0;
+
+    for (let i = 0; i < marks.length; i++) {
+        sumItem += marks[i];
+    }
+
+    let roundedAverage = Math.round(sumItem / marks.length);
+
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const now = Date.now(),
+          yearTimestamp = 31536000000,
+          dayTimestamp = 86400000;
+
+    let userBirthday = new Date(birthday),
+        diff = now - userBirthday.getTime(),
+        days = 0;
+    
+    for (let i = userBirthday.getFullYear(); i < new Date().getFullYear(); i++) {
+        if (isLeapYear(i)) {
+            days++;
+        }
+    }
+
+    let age = diff / (yearTimestamp + (dayTimestamp * days));
+    
+    return age > 18;
+}
+
+
+function isLeapYear(year) {
+    if (!(year % 100)) {
+        if (!(year % 400)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    if (!(year % 4)) {
+        return true;
+    }
+
+    return false;
 }
